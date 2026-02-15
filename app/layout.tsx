1c65bc9558d6e0f03e44cf6b2ca40616618963c8
MeVaishnav <vaishnavcsbedradka@gmail.com>
2026-02-15 14:19:35 +0530

    updated sidebar to have recent chats and rename and delete buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { type Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AppContextProvider } from "@/context/AppContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,13 +26,15 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
-      </html>
+      <AppContextProvider>
+        <html lang="en">
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+            {children}
+          </body>
+        </html>
+      </AppContextProvider>
     </ClerkProvider>
   );
 }
